7af6e44ad0ae24040f684c0111b9b0e7664a4a0e
Thoriq Dhiya Ikbar <thoriqikbar42@gmail.com>
2025-11-12 20:08:06 +0700

    logout

--- FE_kosan/app/admin/akun/page.tsx
+++ FE_kosan/app/admin/akun/page.tsx
@@ -1,15 +1,22 @@
 "use client"
 import ProfileCard from "../../../components/profileCard";
-
+import { useRouter } from "next/navigation"
+import { removeCookie } from "@/lib/client-cookies";
 
 export default function AdminAccountPage() {
+  const router = useRouter();
+  const handleLogout = () => {
+    removeCookie("token"); // hapus cookie token
+    removeCookie("user");  // kalau kamu juga simpan user, hapus juga
+    router.replace("/login");   // redirect ke halaman awal
+  };
   return (
     <ProfileCard
       role="Admin"
       name="Admin Kos"
       email="admin@gmail.com"
       phone="08987654321"
-      onLogout={() => alert("Logout Admin")}
+      onLogout={handleLogout}
     />
   );
 }
